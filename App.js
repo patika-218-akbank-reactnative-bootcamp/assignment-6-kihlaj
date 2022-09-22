e7355e08882ada19b9520c686e8f6f0b38de69b6
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-09-22 04:18:37 +0300

    craeted firebase folder and install packages that will be used

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Provider } from 'react-redux'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Navigation from './src/navigation'
+import store from './src/redux/store'
+
+const App = () => {
+	//Here, the theme and user contexts surround the application.
+	return (
+		<Provider store={store}>
+			<Navigation />
+		</Provider>
+	)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ src/utilities/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA816mq3D0nSNGs_NiQa6RaMu7gevghuBA',
+	authDomain: 'snapchat-839b8.firebaseapp.com',
+	projectId: 'snapchat-839b8',
+	storageBucket: 'snapchat-839b8.appspot.com',
+	messagingSenderId: '86096734098',
+	appId: '1:86096734098:web:5bbeca34df79e72055034b',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
